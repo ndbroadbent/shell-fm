33a734626b7a2f4f3bb7ada1798751a5fe43789f
Nathan B <nathan.f77@gmail.com>
2010-04-11 16:21:04 +1200

    added script to compile and build tinycore extension for shell-fm

---
+++ create_tinycore_pkg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
+CDIR=`pwd`
+
+PKG_NAME="shell-fm"
+
+make clean
+make
+
+touch /tmp/mark
+make DESTDIR=/tmp/$PKG_NAME install
+
+cd /tmp
+mksquashfs $PKG_NAME $PKG_NAME.tcz
+cd /tmp/$PKG_NAME
+mv ../$PKG_NAME.tcz .
+
+find usr -not -type d > $PKG_NAME.tcz.list
+
+rm -rf usr
+
+mv /tmp/$PKG_NAME.tcz /tmp/$PKG_NAME.tcz.list $CDIR
